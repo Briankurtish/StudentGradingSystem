f06cf4d27a1757de65fab2c76b3f45563ebed496
cipher <asaforbrian18@gmail.com>
2024-04-19 18:42:25 +0300

    created Grades class

---
+++ src/studentgradingsystem/Grades.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentgradingsystem;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Cipher
+ */
+public class Grades implements Serializable{
+    
+}
