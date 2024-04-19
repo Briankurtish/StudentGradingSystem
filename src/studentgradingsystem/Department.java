998d6a13ac83481d3593ddee1360a78ba4edf579
cipher <asaforbrian18@gmail.com>
2024-04-19 18:41:35 +0300

    added getters and setters for the Department class

--- src/studentgradingsystem/Department.java
+++ src/studentgradingsystem/Department.java
@@ -19,6 +19,22 @@ public class Department implements Serializable{
         this.dept_id = dept_id;
         this.dept_name = dept_name;
     }
+
+    public int getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(int dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
     
     
     
