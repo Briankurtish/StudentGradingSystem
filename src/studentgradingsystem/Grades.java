33be6e4ae10292a60627f4b4835d51fc6413af58
cipher <asaforbrian18@gmail.com>
2024-04-19 18:45:54 +0300

    created the Grade constructor

--- src/studentgradingsystem/Grades.java
+++ src/studentgradingsystem/Grades.java
@@ -18,6 +18,16 @@ public class Grades implements Serializable{
     private float grd_hw; 
     private float grd_final;
     private String grd_Igrade;
+
+    public Grades(int grd_id, int std_id, int crs_id, float grd_mt, float grd_hw, float grd_final, String grd_Igrade) {
+        this.grd_id = grd_id;
+        this.std_id = std_id;
+        this.crs_id = crs_id;
+        this.grd_mt = grd_mt;
+        this.grd_hw = grd_hw;
+        this.grd_final = grd_final;
+        this.grd_Igrade = grd_Igrade;
+    }
     
     
 
