5c39f234dff606320631ca441dde45ab20f80377
cipher <asaforbrian18@gmail.com>
2024-04-19 18:56:48 +0300

    added getters and setters for the attendance class

--- src/studentgradingsystem/Attendance.java
+++ src/studentgradingsystem/Attendance.java
@@ -23,6 +23,40 @@ public class Attendance implements Serializable{
         this.crs_id = crs_id;
         this.att_date = att_date;
     }
+
+    public int getAtt_id() {
+        return att_id;
+    }
+
+    public void setAtt_id(int att_id) {
+        this.att_id = att_id;
+    }
+
+    public int getStd_id() {
+        return std_id;
+    }
+
+    public void setStd_id(int std_id) {
+        this.std_id = std_id;
+    }
+
+    public int getCrs_id() {
+        return crs_id;
+    }
+
+    public void setCrs_id(int crs_id) {
+        this.crs_id = crs_id;
+    }
+
+    public Date getAtt_date() {
+        return att_date;
+    }
+
+    public void setAtt_date(Date att_date) {
+        this.att_date = att_date;
+    }
+    
+    
     
     
 }
