5f6b1749ddd8a4af82eafead37dceedd8f99f1c2
cipher <asaforbrian18@gmail.com>
2024-04-19 18:40:54 +0300

    created the department constructor

--- src/studentgradingsystem/Department.java
+++ src/studentgradingsystem/Department.java
@@ -4,11 +4,21 @@
  */
 package studentgradingsystem;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Cipher
  */
-public class Department {
+public class Department implements Serializable{
+    
+    private int dept_id;
+    private String dept_name;
+
+    public Department(int dept_id, String dept_name) {
+        this.dept_id = dept_id;
+        this.dept_name = dept_name;
+    }
     
     
     
